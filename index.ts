89ea85c167a8c1b84479f2b73abe69e429eb2ee9
Dreamystify <support@dreamystify.com>
2022-08-16 13:34:32 +1000

    Add tasks to run

--- index.ts
+++ index.ts
@@ -1,8 +1,15 @@
 import { task1 } from './task1';
+import { task2 } from './task2';
+import { task3 } from './task3';
+import { task4 } from './task4';
+import { task5 } from './task5';
 
 const hello = async (): Promise<void> => {
   console.log('Hello World');
   await task1();
-
+  const response = await task2();
+  const data = task3(response.data);
+  await task4(data);
+  await task5();
 }
 hello();
