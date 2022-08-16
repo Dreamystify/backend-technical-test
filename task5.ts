e7c87be7f79f6cae813737bfc0defb8c19303e7a
Dreamystify <support@dreamystify.com>
2022-08-16 13:34:07 +1000

    Add s3 upload function for task 5

--- task5.ts
+++ task5.ts
@@ -1,7 +1,27 @@
-import * as dotenv from 'dotenv'
+import * as dotenv from 'dotenv';
+import { S3 } from 'aws-sdk';
+import fs from 'fs';
 
 export const task5 = async (): Promise<void> => {
+  console.log(`Uploading csv to s3 for task 5`);
   dotenv.config();
-  console.log(`AWS_ACCESS_KEY_ID=${process.env.AWS_ACCESS_KEY_ID}`);
-  console.log(`AWS_SECRET_ACCESS_KEY=${process.env.AWS_SECRET_ACCESS_KEY}`);
+
+  const s3 = new S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  });
+
+  const params = {
+    Bucket: 'inquisitive-backend-developer-tests',
+    Key: 'Corey-Lylyk/output.csv',
+    Body: fs.readFileSync('./csv/output.csv')
+  };
+
+  s3.putObject(params, function (err: Error, data) {
+    if (err) {
+      console.log(err, err.stack);
+    } else {
+      console.log(`successfully uploaded file`);
+    }
+  });
 }
