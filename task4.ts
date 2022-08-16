774710db58576614294ae69078bb7c0d03e174f4
Dreamystify <support@dreamystify.com>
2022-08-16 13:33:11 +1000

    Add csv saving function for task 4

---
+++ task4.ts
@@ -0,0 +1,18 @@
+import converter from 'json-2-csv';
+import fs from 'fs';
+
+export const task4 = async (data: any[]): Promise<void> => {
+    console.log(`Saving csv data for task 4`);
+
+    const options = {
+        prependHeader: true,
+        keys: ['Year', 'Subject', 'Lesson'],
+    };
+
+    const csv = await converter.json2csvAsync(data, options);
+    if (!csv) {
+      console.log('Error formatting data.');
+    }
+
+    const outputFile = fs.writeFileSync(`./csv/output.csv`, csv);
+};
